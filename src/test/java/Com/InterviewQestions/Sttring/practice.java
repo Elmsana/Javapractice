04832d8a8f2ff9f7a2b81b8f34fe3a34079910b1
amauelbri <amanueltzerea@gmail.com>
2020-11-13 21:23:49 -0500

    aman

---
+++ src/test/java/Com/InterviewQestions/Sttring/practice.java
@@ -0,0 +1,21 @@
+package Com.InterviewQestions.Sttring;
+
+
+      class practice {
+          public static void main(String[] args) {
+              int number[] = {12, 13, 42, 32, 15, 156, 23, 51, 12};
+              int max = findMax(number);
+          }
+
+              public int findMax ( int[] numbers){
+          }
+
+              int max = 0;
+
+
+              return max;
+
+          }
+
+        }
+
